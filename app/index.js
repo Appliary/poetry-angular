4507288a66963099ce28d35c11833176cd8afa65
Michaël Carrette <miccarr@me.com>
2016-10-02 15:56:41 +0200

    appName

--- app/index.js
+++ app/index.js
@@ -1,6 +1,9 @@
 var app = angular.module( __appName, __appDependencies );
 
 app.config( function ( $locationProvider, $httpProvider ) {
-    $locationProvider.html5Mode( true );
-    $httpProvider.defaults.withCredentials = true;
-} );
+        $locationProvider.html5Mode( true );
+        $httpProvider.defaults.withCredentials = true;
+    } )
+    .run( function ( $rootScope ) {
+        $rootScope.__appName = __appName;
+    } );
