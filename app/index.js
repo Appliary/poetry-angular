1ad1202d7ebfa40e83ba6a22f25903ea7caeed99
kliment@kodar.net <kliment@kodar.net>
2016-10-06 15:30:03 +0300

    - Commented out temporarily the custom routes incuding code

--- app/index.js
+++ app/index.js
@@ -4,28 +4,28 @@ app.config( function ( $locationProvider, $httpProvider, $stateProvider, $urlRou
         $locationProvider.html5Mode( true );
         $httpProvider.defaults.withCredentials = true;
         
-        (function loadCustomRoutes() {
-            $http.get( '/' + __appName + '/__routes.json' )
-                .then(function (result) {
-                    var Routes = {};
+        // (function loadCustomRoutes() {
+        //     $http.get( '/' + __appName + '/__routes.json' )
+        //         .then(function (result) {
+        //             var Routes = {};
 
-                    for (var key in result.data) {
-                        var _route = result.data[key];
-                        Routes[key] = _route;
-                    }
+        //             for (var key in result.data) {
+        //                 var _route = result.data[key];
+        //                 Routes[key] = _route;
+        //             }
 
-                    $urlRouterProvider.otherwise( '/404' );
-                    Object.keys( Routes )
-                        .forEach( function ( route ) {
-                            console.log('Registering route: ' + route);
-                            $stateProvider.state( route, Routes[ route ] );
-                        });
+        //             $urlRouterProvider.otherwise( '/404' );
+        //             Object.keys( Routes )
+        //                 .forEach( function ( route ) {
+        //                     console.log('Registering route: ' + route);
+        //                     $stateProvider.state( route, Routes[ route ] );
+        //                 });
 
-                })
-                .catch(function (err) {
-                    console.log('No custom routes found.')
-                });
-        }) ();
+        //         })
+        //         .catch(function (err) {
+        //             console.log('No custom routes found.')
+        //         });
+        // }) ();
     } )
     .run( function ( $rootScope ) {
         $rootScope.__appName = __appName;
