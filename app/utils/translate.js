ac43d686bacd118cc8352605aecd636c9d82faea
Michaël Carrette <miccarr@me.com>
2016-10-13 11:01:55 +0200

    pot

--- app/utils/translate.js
+++ app/utils/translate.js
@@ -1,9 +1,37 @@
-var i18n_registry = {};
+var i18n_registry = {},
+    pot = function pot() {
+        pot.active = true
+        var out = '# ' + Date.now() + '\nmsgid ""\nmsgstr ""\n\n\n' ;
+        pot.registry.forEach( function ( k ) {
+            out += 'msgid "' + k + '"\nmsgstr ""\n\n';
+        } );
+        window.location.replace(
+            'data:application/octet-stream,' + encodeURIComponent( out )
+        );
+    };
+
+pot.registry = [];
+pot.active = false;
+pot.activate = function () {
+    pot.active = true
+};
 
 app.filter( 'translate', function () {
+
     function translate( key ) {
 
         if ( !key ) return undefined;
+
+        var k = key;
+        do {
+            if ( !~pot.registry.indexOf( k ) )
+                pot.registry.push( k );
+            k = k.split( ':' )
+                .slice( 0, -1 )
+                .join( ':' );
+        } while ( ~k.indexOf( ':' ) );
+
+        if ( pot.active ) return key;
 
         if ( i18n_registry[ key ] !== undefined )
             return i18n_registry[ key ];
