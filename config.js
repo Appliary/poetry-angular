c7facc80b7b2cc8d25389ff70f611a7894284188
kliment@kodar.net <kliment@kodar.net>
2017-01-27 11:10:59 +0200

    - Added processing of poetry-angular's own dependencies

--- config.js
+++ config.js
@@ -1,47 +1,54 @@
 const fs = require( 'fs' ),
     Poetry = require( 'poetry' ),
-    poetryAngularConf = './node-modules/poetry-angular/config';
+    poetryAngularConf = './node_modules/poetry-angular/config';
 
 let files = [],
     poaFiles = [],
     config = require('./default.json');
+
+let _processFiles = (files, parentFolder) => {
+    files.forEach( ( file ) => {
+        let name = file.split( '.', 1 )[ 0 ];
+        try {
+
+            let imp = require( parentFolder + file );
+
+            if ( !config[ name ] ) {
+                if( imp instanceof Array )
+                    config[ name ] = [];
+                else
+                    config[ name ] = {};
+            }
+
+            Object.keys( imp )
+                .forEach( ( key ) => {
+                    if( imp instanceof Array )
+                        config[ name ].push( imp[ key ] );
+                    else
+                        config[ name ][ key ] = imp[ key ];
+                } );
+
+        } catch ( err ) {
+            Poetry.log.error( 'Unable to require configFile', file )
+        }
+    } );
+}
 
 try {
     debugger;
     files = fs.readdirSync( './config' );
 
     Poetry.log.info('Attempting to read poetry-angular dependencies');
-    //poaFiles = require(poetryAngularConf);
+    poaFiles = fs.readdirSync(poetryAngularConf);
     Poetry.log.info('Sucessfully read the poetry-angular files: ', poaFiles)
 } catch ( err ) {
     throw Poetry.log.error( 'Unable to access config directory', err );
 }
 
-files.forEach( ( file ) => {
-    debugger;
-    let name = file.split( '.', 1 )[ 0 ];
-    try {
+// Process first the poetry-angular's own dependencies
+_processFiles(poaFiles, './config/')
 
-        let imp = require( '../../config/' + file );
-
-        if ( !config[ name ] ) {
-            if( imp instanceof Array )
-                config[ name ] = [];
-            else
-                config[ name ] = {};
-        }
-
-        Object.keys( imp )
-            .forEach( ( key ) => {
-                if( imp instanceof Array )
-                    config[ name ].push( imp[ key ] );
-                else
-                    config[ name ][ key ] = imp[ key ];
-            } );
-
-    } catch ( err ) {
-        Poetry.log.error( 'Unable to require configFile', file )
-    }
-} );
+// Process then the app's dependencies
+_processFiles(files, '../../config/')
 
 module.exports = config;
