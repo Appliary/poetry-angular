3e169fefb61e1c537269467e874a44b2c1e3cf63
Michaël Carrette <miccarr@me.com>
2017-03-02 17:06:59 +0100

    isNaN case

--- app/generic/list.js
+++ app/generic/list.js
@@ -67,7 +67,7 @@ app.controller( 'generic/list', function ( $scope, $http, $location, ngDialog, $
         $http.get( url )
             .then( function success( response ) {
 
-                if ( $scope.reqID != reqID )
+                if ( !isNan( reqID ) && $scope.reqID != reqID )
                     return console.warn( 'Response dropped', $scope.reqID, reqID );
 
                 isLoading = false;
