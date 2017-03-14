1d384b9610dde8ccfa3631e6be5009ca81b82243
Michaël Carrette <miccarr@me.com>
2017-03-14 14:37:19 +0100

    debug

--- app/devices/devices.service.js
+++ app/devices/devices.service.js
@@ -1,144 +1,132 @@
-app.factory('deviceService', deviceService);
+app.factory( 'deviceService',
+    function deviceService(
+        $http,
+        $q,
+        $rootScope
+    ) {
 
-deviceService.$inject = ['$http', '$q', '$rootScope'];
+        var factory = {
 
-/* @ngInject */
-function deviceService($http, $q, $rootScope) {
+            getAllDevices: getAllDevices,
+            addNewDevice: addNewDevice,
+            transformDataFormToApi: transformDataFormToApi,
+            updateDevice: updateDevice,
+            getDeviceMeasurements: getDeviceMeasurements,
 
-    var factory = {
+            getDevicesPositions: getDevicesPositions,
+            getDevicePositions: getDevicePositions
 
-        getAllDevices: getAllDevices,
-        addNewDevice : addNewDevice,
-        transformDataFormToApi: transformDataFormToApi,
-        updateDevice : updateDevice,
-        getDeviceMeasurements : getDeviceMeasurements,
-
-        getDevicesPositions: getDevicesPositions,
-        getDevicePositions: getDevicePositions
-
-    };
-    return factory;
-
-    function getDevicePositions ( idDevice ) {
-        var defer = $q.defer();
-        $http.get('/api/devices/'+ idDevice + '/positions')
-            .then(function (response) {
-                defer.resolve( response.data );
-            })
-            .catch(function (error) {
-                defer.reject(error);
-            });
-        return defer.promise;
-    }
-
-
-    function getAllDevices() {
-
-        var defer = $q.defer();
-        $http.get('/api/devices')
-            .then(function (response) {
-                defer.resolve( response.data );
-            })
-            .catch(function (error) {
-                defer.reject(error);
-            });
-
-        return defer.promise;
-    }
-
-
-    function addNewDevice (body) {
-        var defer = $q.defer();
-        $http.post('/api/devices', body)
-            .then(function (response) {
-                defer.resolve( response );
-            })
-            .catch(function (error) {
-                defer.reject(error);
-            });
-        return defer.promise;
-    }
-
-    function transformDataFormToApi (body) {
-        var d = new Date();
-        var timestamp = d.getTime();
-
-        var newDevice = {
-            timestamp: timestamp,
-            createdAt: timestamp,
-            team: $rootScope.session.team,
-            _id: setEmptyDataToUndefined(body.id),
-            status: setEmptyDataToUndefined(body.status),
-            type: setEmptyDataToUndefined(body.type),
-
-            datalogger: setEmptyDataToUndefined(body.datalogger),
-            name: setEmptyDataToUndefined(body.name),
-            model: setEmptyDataToUndefined(body.model),
-            brand: setEmptyDataToUndefined(body.brand),
-            notes: setEmptyDataToUndefined(body.notes),
-            timeout: setEmptyDataToUndefined(body.timeout)
         };
+        return factory;
+
+        function getDevicePositions( idDevice ) {
+            var defer = $q.defer();
+            $http.get( '/api/devices/' + idDevice + '/positions' )
+                .then( function ( response ) {
+                    defer.resolve( response.data );
+                } )
+                .catch( function ( error ) {
+                    defer.reject( error );
+                } );
+            return defer.promise;
+        }
 
 
-        /**
-         * Need to set to undefined empty input, because the DB refuse to accept an empty string
-         */
-        function setEmptyDataToUndefined(data){
-            if( data === "" ){
-                data = undefined
+        function getAllDevices() {
+
+            var defer = $q.defer();
+            $http.get( '/api/devices' )
+                .then( function ( response ) {
+                    defer.resolve( response.data );
+                } )
+                .catch( function ( error ) {
+                    defer.reject( error );
+                } );
+
+            return defer.promise;
+        }
+
+
+        function addNewDevice( body ) {
+            var defer = $q.defer();
+            $http.post( '/api/devices', body )
+                .then( function ( response ) {
+                    defer.resolve( response );
+                } )
+                .catch( function ( error ) {
+                    defer.reject( error );
+                } );
+            return defer.promise;
+        }
+
+        function transformDataFormToApi( body ) {
+            var d = new Date();
+            var timestamp = d.getTime();
+
+            var newDevice = {
+                timestamp: timestamp,
+                createdAt: timestamp,
+                team: $rootScope.session.team,
+                _id: setEmptyDataToUndefined( body.id ),
+                status: setEmptyDataToUndefined( body.status ),
+                type: setEmptyDataToUndefined( body.type ),
+
+                datalogger: setEmptyDataToUndefined( body.datalogger ),
+                name: setEmptyDataToUndefined( body.name ),
+                model: setEmptyDataToUndefined( body.model ),
+                brand: setEmptyDataToUndefined( body.brand ),
+                notes: setEmptyDataToUndefined( body.notes ),
+                timeout: setEmptyDataToUndefined( body.timeout )
+            };
+
+
+            /**
+             * Need to set to undefined empty input, because the DB refuse to accept an empty string
+             */
+            function setEmptyDataToUndefined( data ) {
+                if ( data === "" ) data = undefined;
+                return data;
             }
-            return data
+
+            return newDevice;
         }
 
-        return newDevice;
-    }
-
-    function updateDevice ( idDevice, data ){
-        var defer = $q.defer();
-        $http.put('/api/devices/'+ idDevice, data )
-            .then(function (response) {
-                defer.resolve( response.data );
-            })
-            .catch(function (error) {
-                defer.reject(error);
-            });
-        return defer.promise;
-    }
-
-    function getDeviceMeasurements ( idDevice ) {
-        var defer = $q.defer();
-        $http.get( '/api/devices/' + idDevice + '/measurements' )
-            .then(function (response) {
-                defer.resolve(response)
-            })
-            .catch(function (error) {
-                defer.reject(error)
-            });
-        return defer.promise;
-    }
-
-    function getDevicesPositions ( params ) {
-        var defer = $q.defer();
-
-        var string = "?";
-        for(var i in params){
-            string += i;
-            string += "=";
-            string += params[i];
-            string += "&"
+        function updateDevice( idDevice, data ) {
+            var defer = $q.defer();
+            $http.put( '/api/devices/' + idDevice, data )
+                .then( function ( response ) {
+                    defer.resolve( response.data );
+                } )
+                .catch( function ( error ) {
+                    defer.reject( error );
+                } );
+            return defer.promise;
         }
-        string = string.substr(0, string.length-1);
 
-        $http.get( '/api/devices/positions' + string )
-            .then(function (response) {
-                defer.resolve(response)
-            })
-            .catch(function (error) {
-                defer.reject(error)
-            });
-        return defer.promise;
-    }
+        function getDeviceMeasurements( idDevice ) {
+            var defer = $q.defer();
+            $http.get( '/api/devices/' + idDevice + '/measurements' )
+                .then( defer.resolve )
+                .catch( defer.reject );
+            return defer.promise;
+        }
 
-}
+        function getDevicesPositions( params ) {
+            var defer = $q.defer();
 
+            var string = "?";
+            for ( var i in params ) {
+                string += i;
+                string += "=";
+                string += params[ i ];
+                string += "&";
+            }
+            string = string.substr( 0, string.length - 1 );
 
+            $http.get( '/api/devices/positions' + string )
+                .then( defer.resolve )
+                .catch( defer.reject );
+            return defer.promise;
+        }
+
+    } );
--- app/devices/rules.js
+++ app/devices/rules.js
@@ -1,14 +1,15 @@
 app.controller( 'devices/rules', function ( $scope, $http ) {
 
-    function getRules(){
-        $http.get( $scope.$root.__module.api + '/' + $scope.__id + '/rules')
+    function getRules() {
+        $http.get( $scope.$root.__module.api + '/' + $scope.__id + '/rules' )
             .then( function success( response ) {
 
                 $scope.rules = response.data.data;
+                $scope.total = response.data.data.length;
 
             } );
     }
 
-    $scope.$watch('__id', getRules);
+    $scope.$watch( '__id', getRules );
 
 } );
