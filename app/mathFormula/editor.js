7271d2327295acf45886f4671dba9dcb21f90d9c
Michaël Carrette <miccarr@me.com>
2017-03-23 11:19:13 +0100

    cl

--- app/mathFormula/editor.js
+++ app/mathFormula/editor.js
@@ -11,17 +11,20 @@ app.controller( 'mathFormula/editor', function (
          * Open a modal to create a new input for the formula
          */
         add: function add() {
+
+            var scope = $scope.$new();
+            scope.inputs = $scope.item.inputs;
+
             ngDialog.openConfirm( {
                     templateUrl: 'mathFormula/add.pug',
                     controller: 'mathFormula/add',
-                    scope: {
-                        inputs: $scope.item.inputs
-                    }
+                    scope: scope
                 } )
                 .then( function success( input ) {
                     console.info( input );
                     $scope.item.inputs.push( input );
                 }, function () {} );
+
         },
 
         remove: function () {}
