8ac0018f77169d75f1d0dc0316b9fe2dfe22b39f
Michaël Carrette <miccarr@me.com>
2017-03-28 09:43:52 +0200

    predefine vars

--- app/mathFormula/editor.js
+++ app/mathFormula/editor.js
@@ -59,6 +59,10 @@ app.controller( 'mathFormula/editor', function (
         $scope.inputValues = {};
         if ( !$scope.item || !$scope.item.inputs || !$scope.item.inputs.length ) return;
 
+        $scope.item.inputs.forEach( function fe( input ) {
+            $scope.inputValues[ input.varName ] = 0;
+        } );
+
         $http.post( '/api/rules/getVars', {
                 inputs: $scope.item.inputs
             } )
