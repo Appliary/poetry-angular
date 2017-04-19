4736860638ca59fa5b8d7ec48e8137f032f8fce8
Stephane <sdemingongo@gmail.com>
2017-04-19 13:15:18 +0100

    localize update

--- app/utils/l10n.js
+++ app/utils/l10n.js
@@ -2,10 +2,43 @@ app.filter( 'localize', function ( $filter, $rootScope ) {
 
     function localize( input, kind ) {
 
-        if ( kind == 'weekly' ) return ( new Date( input ) )
-            .getWeek();
-        if ( kind == 'monthly' ) return ( new Date( input ) )
-            .getMonth() + 1;
+        if ( kind == 'daily' ) {
+          var val = _date(input);
+          return val.substring(0, 11);
+        }
+        if ( kind == 'weekly' ) {
+          var inputDate = new Date( input );
+          var newYear = new Date(inputDate.getFullYear(),0,1);
+          var day = newYear.getDay();
+          day = (day >= 0 ? day : day + 7);
+          var daynum = Math.floor((inputDate.getTime() - newYear.getTime() -
+          (inputDate.getTimezoneOffset()-newYear.getTimezoneOffset())*60000)/86400000) + 1;
+          var weeknum;
+          //if the year starts before the middle of a week
+          if(day < 4) {
+            weeknum = Math.floor((daynum+day-1)/7) + 1;
+            if(weeknum > 52) {
+              nYear = new Date(inputDate.getFullYear() + 1,0,1);
+              nday = nYear.getDay();
+              nday = nday >= 0 ? nday : nday + 7;
+              weeknum = nday < 4 ? 1 : 53;
+            }
+          }
+          else {
+            weeknum = Math.floor((daynum+day-1)/7);
+          }
+          return inputDate.getFullYear()+"-W"+(weeknum < 10 ? "0" : "")+weeknum+"-1";
+        }
+        if ( kind == 'monthly' ) {
+          /*return ( new Date( input ) )
+            .getMonth() + 1;*/
+            return ( new Date( input ) ).toLocaleString($rootScope.user.language, { month: "short" }) + " " + ( new Date( input ) ).getFullYear();
+        }
+        if ( kind == 'yearly' ) {
+          /*return ( new Date( input ) )
+            .getMonth() + 1;*/
+            return ( new Date( input ) ).getFullYear();
+        }
         if ( kind ) return _date( input );
 
         if ( input === undefined ) return undefined;
