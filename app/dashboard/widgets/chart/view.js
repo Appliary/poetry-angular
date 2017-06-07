328e8379344420ade1a601685abe320e89c7b381
Stephane <sdemingongo@gmail.com>
2017-06-07 14:46:17 +0100

    getData + readInputs

--- app/dashboard/widgets/chart/view.js
+++ app/dashboard/widgets/chart/view.js
@@ -1,6 +1,7 @@
 app.controller( 'dashboard/widgets/chart/view', function ChartWidget(
     $scope,
-    $http
+    $http,
+    $q
 ) {
 
     $scope.chartObject = {
@@ -69,7 +70,7 @@ app.controller( 'dashboard/widgets/chart/view', function ChartWidget(
                 break;
 
             default:
-                throw new Error( 'Unknown timeFrame type' );
+                throw new Error( 'Unknown timeFrame type ', tf.unit);
         }
 
         return {
@@ -87,10 +88,75 @@ app.controller( 'dashboard/widgets/chart/view', function ChartWidget(
      * Pour les dates : $scope.timeFrame et $scope.widget.options.step
      ***/
 
-    $http.get( '/api/measurements' )
+    /*$http.get( '/api/measurements' )
         .then( function receivedData( res ) {
-            chartObject.data = res.data.data;
-        } );
+            $scope.chartObject.data = res.data.data;
+    } );*/
+
+    // get measurements for one input
+    function getData(input, after, before){
+      var dfd = $q.defer();
+      var apiUrl = input.kind == 'smartdevice' ? '/api/smartdevices/' : '/api/devices/';
+      var aggregation = ( input.kind == 'smartdevice' && $scope.widget.options.step ) ? '/' + $scope.widget.options.step : "";
+      var url = apiUrl + input.id +'/measurements' + aggregation + '?before=' + before + '&after=' + after + '&sort=asc';
+
+      var data = [];
+      var result = {};
+      console.log( "getdevicesdata url", url );
+      $http.get( url )
+      .then( function ( response ) {
+        if (angular.isObject(response.data) && response.data.data ) {
+          result.name = response.data.parent.name;
+          result.unit = response.data.parent.unit || "";
+          var measurementData = response.data.data;
+          measurementData.forEach( function ( md ) {
+            var found = false;
+            if ( angular.isArray(md.measurements) ) {
+              md.measurements.forEach( function ( measurement ) {
+                if ( measurement.type == input.type && !found ) {
+                  found = true;
+                  var date = new Date( md.timestamp );
+                  var dateToShow = date.getHours() + ' - ' + date.getDate() + '/' + date.getMonth();
+                  data.push( [ date, measurement.value ] );
+                }
+              } );
+            }
+          } );
+        }
+        else {
+          data = response.data;
+        }
+        result.data = data;
+        dfd.resolve( result );
+      },
+      function(e){
+        console.error(e);
+        dfd.reject( e );
+      }
+    );
+
+      return dfd.promise;
+    }
+
+    function readInputs(){
+          if(angular.isArray($scope.widget.options.inputs)){
+            $scope.widget.options.inputs.forEach(
+              function(input){
+                getData(input, $scope.timeFrame.from, $scope.timeFrame.to).then(
+                  function(res){
+                    var head = ['date', res.name || ""];
+                    var chartData = res.data;
+                    chartData.unshift(head);
+                    console.log("data",chartData);
+                    $scope.chartObject.data = chartData;
+                  }
+                );
+              }
+            );
+          }
+    }
+
+    readInputs();
 
 
 } );
