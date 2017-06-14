b1a4c4e4d135a71f68ee6345cfb4146f9cd9da0a
Michaël Carrette <miccarr@me.com>
2017-06-14 17:46:18 +0200

    dbg table

--- app/dashboard/widgets/chart/view.js
+++ app/dashboard/widgets/chart/view.js
@@ -6,181 +6,208 @@ app.controller( 'dashboard/widgets/chart/view', function ChartWidget(
     googleChartApiConfig
 ) {
 
-  /**
-   * For some charts, only show last measurement
-   */
-  var isSingleDataChart = false;
+    $scope.tgtab = {
+        isTable: false
+    };
+
+    $scope.toggleTable = function toggleTable() {
+        console.log( 'tgtab' );
+        if ( $scope.tgtab.isTable ) {
+            $scope.tgtab.isTable = false;
+            init();
+        } else {
+            $scope.tgtab.chartOptions = angular.copy(
+                $scope.widget.options.chartOptions
+            );
+            $scope.tgtab.chartType = angular.copy(
+                $scope.widget.options.chartType
+            );
+            $scope.widget.options.chartType = 'Table';
+            $scope.widget.options.chartOptions = {
+                width: '100%'
+            };
+            $scope.tgtab.isTable = true;
+            init();
+            $scope.widget.options.chartOptions = $scope.tgtab.chartOptions;
+            $scope.widget.options.chartType = $scope.tgtab.chartType;
+        }
+    };
+
+    /**
+     * For some charts, only show last measurement
+     */
+    var isSingleDataChart = false;
 
     init();
 
     $scope.widget.refresh = init;
 
     /**
-    * Init function
-    */
-    function init(){
-      console.log("init");
-      delete $scope.widget.options.chartOptions.width;
-      delete $scope.widget.options.chartOptions.height;
-      $scope.widget.chartObject = {
-          data: [],
-          type: $scope.widget.options.chartType,
-          options: $scope.widget.options.chartOptions
-      };
+     * Init function
+     */
+    function init() {
+        console.log( "init" );
+        delete $scope.widget.options.chartOptions.width;
+        delete $scope.widget.options.chartOptions.height;
+        $scope.widget.chartObject = {
+            data: [],
+            type: $scope.widget.options.chartType,
+            options: $scope.widget.options.chartOptions
+        };
 
-      /**
-       * set user locale and language or set default
-       */
-      var locale = $rootScope.user && $rootScope.user.locale ? $rootScope.user.locale : 'us';
-      var language = $rootScope.user && $rootScope.user.language ? $rootScope.user.language : 'en';
-      googleChartApiConfig.optionalSettings.locale = locale;
-      googleChartApiConfig.optionalSettings.language = language;
+        /**
+         * set user locale and language or set default
+         */
+        var locale = $rootScope.user && $rootScope.user.locale ? $rootScope.user.locale : 'us';
+        var language = $rootScope.user && $rootScope.user.language ? $rootScope.user.language : 'en';
+        googleChartApiConfig.optionalSettings.locale = locale;
+        googleChartApiConfig.optionalSettings.language = language;
 
-      switch ( $scope.widget.chartObject.type ) {
-          case "Gauge":
-          case "PieChart":
-              isSingleDataChart = true;
-              break;
-      }
+        switch ( $scope.widget.chartObject.type ) {
+            case "Gauge":
+            case "PieChart":
+                isSingleDataChart = true;
+                break;
+        }
 
-      /**
-       * timeFrame
-       * Object containing computed from and to
-       */
-      $scope.timeFrame = ( function timeFrame( tf ) {
+        /**
+         * timeFrame
+         * Object containing computed from and to
+         */
+        $scope.timeFrame = ( function timeFrame( tf ) {
 
-          var fromDate = new Date(),
-              toDate = new Date();
+            var fromDate = new Date(),
+                toDate = new Date();
 
-          if ( !tf ) throw new Error( 'No timeframe, aborting' );
+            if ( !tf ) throw new Error( 'No timeframe, aborting' );
 
-          switch ( tf.type ) {
+            switch ( tf.type ) {
 
-              case 'static':
-                  return {
-                      from: tf.from,
-                      to: tf.to
-                  };
+                case 'static':
+                    return {
+                        from: tf.from,
+                        to: tf.to
+                    };
 
-              case 'absolute':
-                  fromDate.setHours( 0, 0, 0, 0 );
-                  toDate.setHours( 0, 0, 0, 0 );
-                  switch ( tf.frame ) {
+                case 'absolute':
+                    fromDate.setHours( 0, 0, 0, 0 );
+                    toDate.setHours( 0, 0, 0, 0 );
+                    switch ( tf.frame ) {
 
-                      case 'today':
-                          toDate.setDate( toDate.getDate() + 1 );
-                          break;
+                        case 'today':
+                            toDate.setDate( toDate.getDate() + 1 );
+                            break;
 
-                      case 'yesterday':
-                          fromDate.setDate( fromDate.getDate() - 1 );
-                          break;
+                        case 'yesterday':
+                            fromDate.setDate( fromDate.getDate() - 1 );
+                            break;
 
-                      case 'thisWeek':
-                          fromDay = fromDate.getDay();
-                          if ( fromDay ) fromDay--;
-                          else fromDay = 6;
+                        case 'thisWeek':
+                            fromDay = fromDate.getDay();
+                            if ( fromDay ) fromDay--;
+                            else fromDay = 6;
 
-                          fromDate.setDate( fromDate.getDate() - fromDay );
-                          toDate.setDate( toDate.getDate() - fromDay + 6 );
-                          break;
+                            fromDate.setDate( fromDate.getDate() - fromDay );
+                            toDate.setDate( toDate.getDate() - fromDay + 6 );
+                            break;
 
-                      case 'lastWeek':
-                          fromDay = fromDate.getDay();
-                          if ( fromDay ) fromDay--;
-                          else fromDay = 6;
+                        case 'lastWeek':
+                            fromDay = fromDate.getDay();
+                            if ( fromDay ) fromDay--;
+                            else fromDay = 6;
 
-                          fromDate.setDate( fromDate.getDate() - fromDay - 7 );
-                          toDate.setDate( toDate.getDate() - fromDay - 1 );
-                          break;
+                            fromDate.setDate( fromDate.getDate() - fromDay - 7 );
+                            toDate.setDate( toDate.getDate() - fromDay - 1 );
+                            break;
 
-                      case 'thisMonth':
-                          fromDate.setDate( 1 );
-                          toDate.setMonth( toDate.getMonth() + 1 );
-                          toDate.setDate( 1 );
-                          break;
+                        case 'thisMonth':
+                            fromDate.setDate( 1 );
+                            toDate.setMonth( toDate.getMonth() + 1 );
+                            toDate.setDate( 1 );
+                            break;
 
-                      case 'lastMonth':
-                          fromDate.setMonth( fromDate.getMonth() - 1 );
-                          fromDate.setDate( 1 );
-                          toDate.setDate( 1 );
-                          break;
+                        case 'lastMonth':
+                            fromDate.setMonth( fromDate.getMonth() - 1 );
+                            fromDate.setDate( 1 );
+                            toDate.setDate( 1 );
+                            break;
 
-                      case 'thisYear':
-                          fromDate.setMonth( 0, 1 );
-                          toDate.setMonth( 0, 1 );
-                          toDate.setFullYear( toDate.getFullYear() + 1 );
-                          break;
+                        case 'thisYear':
+                            fromDate.setMonth( 0, 1 );
+                            toDate.setMonth( 0, 1 );
+                            toDate.setFullYear( toDate.getFullYear() + 1 );
+                            break;
 
-                      case 'lastYear':
-                          fromDate.setMonth( 0, 1 );
-                          toDate.setMonth( 0, 1 );
-                          fromDate.setFullYear( fromDate.getFullYear() - 1 );
-                          break;
+                        case 'lastYear':
+                            fromDate.setMonth( 0, 1 );
+                            toDate.setMonth( 0, 1 );
+                            fromDate.setFullYear( fromDate.getFullYear() - 1 );
+                            break;
 
-                      default:
-                          throw new Error( 'frame not found' );
+                        default:
+                            throw new Error( 'frame not found' );
 
-                  }
-                  toDate.setMilliseconds( -1 );
-                  break;
+                    }
+                    toDate.setMilliseconds( -1 );
+                    break;
 
-              case 'relative':
-                  switch ( tf.unit ) {
+                case 'relative':
+                    switch ( tf.unit ) {
 
-                      case 'years':
-                          fromDate.setFullYear(
-                              fromDate.getFullYear() - tf.count
-                          );
-                          break;
+                        case 'years':
+                            fromDate.setFullYear(
+                                fromDate.getFullYear() - tf.count
+                            );
+                            break;
 
-                      case 'months':
-                          fromDate.setMonth(
-                              fromDate.getMonth() - tf.count
-                          );
-                          break;
-
-
-                      case 'weeks':
-                          tf.count *= 7;
-                          /* jshint -W086 */ // FALLTROUGH IS NORMAL
-
-                      case 'days':
-                          /* jshint +W086 */ // END FALLTROUGH IS NORMAL
-                          fromDate.setDate(
-                              fromDate.getDate() - tf.count
-                          );
-                          break;
-
-                      case 'hours':
-                          fromDate.setHours(
-                              fromDate.getHours() - tf.count
-                          );
-                          break;
-
-                      case 'minutes':
-                          fromDate.setMinutes(
-                              fromDate.getMinutes() - tf.count
-                          );
-                          break;
-
-                  }
-                  break;
-
-              default:
-                  throw new Error( 'Unknown timeFrame type ', tf.unit );
-          }
-
-          return {
-              from: fromDate,
-              to: toDate
-          };
-
-      } )( $scope.widget.options.timeframe );
+                        case 'months':
+                            fromDate.setMonth(
+                                fromDate.getMonth() - tf.count
+                            );
+                            break;
 
 
-      // if the widget use custom data and not inputs (e.g.: reportManagement)
-      if ( $scope.widget.custom ) readCustom();
-      else readInputs();
+                        case 'weeks':
+                            tf.count *= 7;
+                            /* jshint -W086 */ // FALLTROUGH IS NORMAL
+
+                        case 'days':
+                            /* jshint +W086 */ // END FALLTROUGH IS NORMAL
+                            fromDate.setDate(
+                                fromDate.getDate() - tf.count
+                            );
+                            break;
+
+                        case 'hours':
+                            fromDate.setHours(
+                                fromDate.getHours() - tf.count
+                            );
+                            break;
+
+                        case 'minutes':
+                            fromDate.setMinutes(
+                                fromDate.getMinutes() - tf.count
+                            );
+                            break;
+
+                    }
+                    break;
+
+                default:
+                    throw new Error( 'Unknown timeFrame type ', tf.unit );
+            }
+
+            return {
+                from: fromDate,
+                to: toDate
+            };
+
+        } )( $scope.widget.options.timeframe );
+
+
+        // if the widget use custom data and not inputs (e.g.: reportManagement)
+        if ( $scope.widget.custom ) readCustom();
+        else readInputs();
     }
 
 
@@ -288,7 +315,7 @@ app.controller( 'dashboard/widgets/chart/view', function ChartWidget(
     var doInitVAxisTitle = true;
 
     function readInputs() {
-      console.log("readInputs");
+        console.log( "readInputs" );
         if ( angular.isArray( $scope.widget.options.inputs ) ) {
             $scope.widget.options.inputs.forEach( function ( input ) {
                 getData( input, $scope.timeFrame.from, $scope.timeFrame.to )
