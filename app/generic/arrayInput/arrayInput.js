d000d31bead4e4a41481f61a6944e2bb7cc8d980
Michaël Carrette <miccarr@me.com>
2017-06-21 15:18:08 +0200

    dbg

--- app/generic/arrayInput/arrayInput.js
+++ app/generic/arrayInput/arrayInput.js
@@ -12,7 +12,7 @@ app.directive( 'arrayInput', function arrayInput() {
 
             $scope.tags = [];
             $scope.$watchCollection( 'tags', function watchModel( n, o ) {
-                if ( isLoading || !o ) return;
+                if ( isLoading || !o || o == n ) return;
                 if ( !$scope.array || !$scope.array.push ) $scope.array = [];
                 $scope.tags.forEach( function eachTag( tag ) {
                     $scope.array.push( tag.text );
