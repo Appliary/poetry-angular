02cd952a2bf1df57d280c605dbb27172fd6a1b6d
Michaël Carrette <miccarr@me.com>
2017-06-21 14:57:35 +0200

    dbg

--- app/generic/arrayInput/arrayInput.js
+++ app/generic/arrayInput/arrayInput.js
@@ -1,55 +1,56 @@
-app.directive( 'arrayInput', {
-    templateUrl: 'generic/arrayInput/arrayInput.pug',
-    //transclude: true,
-    scope: {
-        'array': "=",
-        'autocomplete': '&'
-    },
-    controller: 'arrayInput'
-} );
-app.controller( 'arrayInput', function arrayInputCtrl( $scope ) {
+app.directive( 'arrayInput', function arrayInput() {
+    return {
+        templateUrl: 'generic/arrayInput/arrayInput.pug',
+        //transclude: true,
+        scope: {
+            'array': "=",
+            'autocomplete': '&'
+        },
+        controller: function arrayInputCtrl( $scope ) {
 
-    $scope.tags = [];
-    $scope.$watchCollection( 'tags', function watchTags() {
-        if ( !$scope.array || !$scope.array.push ) $scope.array = [];
-        $scope.tags.forEach( function eachTag( tag ) {
-            $scope.array.push( tag.text );
-        } );
-    } );
-
-    $scope.$watchCollection( 'array', function watchModel() {
-        if ( !$scope.array || !$scope.array.map ) $scope.array = [];
-        $scope.tags = $scope.array.map( function mapTags( model ) {
-            if ( model.text ) model = model.text;
-
-            // Not a collection, send raw
-            if ( !~model.indexOf( ':' ) ) return {
-                text: model,
-                collection: '',
-                color: 'transparent'
+            $scope.tags = [];
+            $scope.updateModel = function watchTags() {
+                if ( !$scope.array || !$scope.array.push ) $scope.array = [];
+                $scope.tags.forEach( function eachTag( tag ) {
+                    $scope.array.push( tag.text );
+                } );
             };
 
-            var collection = model.split( ':' )[ 0 ];
-            var hash = 0;
-            for ( i = 0; i < collection.length; i++ ) {
-                chr = collection.charCodeAt( i );
-                hash = ( ( hash << 3 ) - hash ) + chr;
-                hash |= 0;
-            }
-            hash = hash.toString( 16 );
+            $scope.$watchCollection( 'array', function watchModel() {
+                if ( !$scope.array || !$scope.array.map ) $scope.array = [];
+                $scope.tags = $scope.array.map( function mapTags( model ) {
+                    if ( model.text ) model = model.text;
 
-            var color = 'rgba(';
-            color += parseInt( hash.splice( 0, 2 ), 16 ) + ',';
-            color += parseInt( hash.splice( 0, 2 ), 16 ) + ',';
-            color += parseInt( hash.splice( 0, 2 ), 16 ) + ',';
-            color += ',0.3';
+                    // Not a collection, send raw
+                    if ( !~model.indexOf( ':' ) ) return {
+                        text: model,
+                        collection: '',
+                        color: 'transparent'
+                    };
 
-            return {
-                text: model,
-                collection: 'arrayItem-' + collection,
-                color: color || 'transparent'
-            };
-        } );
-    } );
+                    var collection = model.split( ':' )[ 0 ];
+                    var hash = 0;
+                    for ( i = 0; i < collection.length; i++ ) {
+                        chr = collection.charCodeAt( i );
+                        hash = ( ( hash << 3 ) - hash ) + chr;
+                        hash |= 0;
+                    }
+                    hash = hash.toString( 16 );
 
+                    var color = 'rgba(';
+                    color += parseInt( hash.splice( 0, 2 ), 16 ) + ',';
+                    color += parseInt( hash.splice( 0, 2 ), 16 ) + ',';
+                    color += parseInt( hash.splice( 0, 2 ), 16 ) + ',';
+                    color += ',0.3';
+
+                    return {
+                        text: model,
+                        collection: 'arrayItem-' + collection,
+                        color: color || 'transparent'
+                    };
+                } );
+            } );
+
+        }
+    };
 } );
