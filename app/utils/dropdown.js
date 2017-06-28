5e10598983e7db362c7245da1dc237b6c2f1fad4
Anthonny Giroud <giroud.anthonny@gmail.com>
2017-06-28 14:28:44 +0200

    sidebar 1st version

--- app/utils/dropdown.js
+++ app/utils/dropdown.js
@@ -1,33 +1,31 @@
-app.directive('appDropdown', function () {
-    return {
-        restrict: 'C',
-        templateUrl: 'utils/dropdown.pug',
-        transclude: true,
-        bindings: {
-            icon: '@',
-            text: '<'
-        },
-        controller: function ($document, $scope) {
+app.component('appDropdown', {
+    restrict: 'C',
+    templateUrl: 'utils/dropdown.pug',
+    transclude: true,
+    bindings: {
+        icon: '@',
+        text: '<'
+    },
+    controller: function ($document, $scope) {
 
-            var clickOut = function () {
+        var clickOut = function () {
 
-                $scope.$apply(function () {
-                    $scope.open = false;
-                });
-                $document.off('mouseup', clickOut);
-
-            }
-
-            $scope.open = false;
-            $scope.toggle = function () {
-
-                if ($scope.open) return;
-
-                $document.on('mouseup', clickOut);
-                $scope.open = true;
-
-            }
+            $scope.$apply(function () {
+                $scope.open = false;
+            });
+            $document.off('mouseup', clickOut);
 
         }
+
+        $scope.open = false;
+        $scope.toggle = function () {
+
+            if ($scope.open) return;
+
+            $document.on('mouseup', clickOut);
+            $scope.open = true;
+
+        }
+
     }
 });
