2c33a35f34e4b5863c94ac2b4195ebb0fc332811
Stephane <sdemingongo@gmail.com>
2017-07-12 15:30:46 +0100

    context as tag

--- app/alertsList/list-directive.js
+++ app/alertsList/list-directive.js
@@ -101,7 +101,7 @@ app.directive("listDirective", function($http, $location, $timeout, ngDialog, Al
 
       // if is context: {id:..., kind:...}
       scope.displayContext = function(coord){
-        return coord.kind +':'+ coord.id;
+        return [coord.kind +':'+ coord.id];
       }
 
       function getUserLanguage(){
