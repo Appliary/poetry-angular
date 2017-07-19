67335b6ba43771c90b9c2fedc1d50c7a6acfd6c6
Michaël Carrette <miccarr@me.com>
2017-07-19 16:21:48 +0200

    cleanup lists

--- app/router/_router.js
+++ app/router/_router.js
@@ -1,4 +1,4 @@
-app.component('appRouter', {
+app.component( 'appRouter', {
     templateUrl: 'router/_router.pug',
     controller: function (
         $window,
@@ -17,8 +17,8 @@ app.component('appRouter', {
         /**
          * Retrieve the modules from config
          */
-        $http.get('/' + __appName + '/__sidebar.json')
-            .then(function onReceiveModulesList(r) {
+        $http.get( '/' + __appName + '/__sidebar.json' )
+            .then( function onReceiveModulesList( r ) {
                 var modules = {
                     'error': {
                         name: 'error',
@@ -27,65 +27,65 @@ app.component('appRouter', {
                     },
                 };
 
-                r.data.forEach(function (module) {
+                r.data.forEach( function ( module ) {
 
-                    if (typeof module == 'string')
+                    if ( typeof module == 'string' )
                         module = {
                             name: module
                         };
 
-                    if (!module.name)
-                        throw console.error('This module has no name', module);
+                    if ( !module.name )
+                        throw console.error( 'This module has no name', module );
 
-                    if (!module.icon)
+                    if ( !module.icon )
                         module.icon = module.name;
 
-                    if (!module.templateUrl && !module.template) {
-                        if ($templateCache.get(module.name + '/list.pug'))
+                    if ( !module.templateUrl && !module.template ) {
+                        if ( $templateCache.get( module.name + '/list.pug' ) )
                             module.templateUrl = module.name + '/list.pug';
                         else
                             module.templateUrl = 'generic/list.pug';
                     }
 
-                    if (!module.api)
+                    if ( !module.api )
                         module.api = '/api/' + module.name;
 
-                    if (!module.controller) {
+                    if ( !module.controller ) {
                         module.controller = 'generic/list';
                     }
 
-                    if (module.path === undefined)
+                    if ( module.path === undefined )
                         module.path = module.name;
 
-                    modules[module.path] = module;
+                    modules[ module.path ] = module;
 
-                });
+                } );
 
                 $scope.$root.__modules = modules;
 
-                $http.get('/' + __appName + '/__routes.json')
-                    .then(function (result) {
+                $http.get( '/' + __appName + '/__routes.json' )
+                    .then( function ( result ) {
                         var Routes = {};
 
-                        for (var key in result.data) {
-                            var _route = result.data[key];
-                            console.log('Registering route: ' + route);
-                            $customRoutesProvider.addState(key, _route);
+                        for ( var key in result.data ) {
+                            var _route = result.data[ key ];
+                            console.log( 'Registering route: ' + key );
+                            $customRoutesProvider.addState( key, _route );
                         }
 
 
-                        route(null, $window.location.pathname);
-                        $scope.$on('$locationChangeStart', route);
+                        route( null, $window.location.pathname );
+                        $scope.$on( '$locationChangeStart', route );
 
-                    })
-                    .catch(function (err) {
-                        console.log('No custom routes found.');
+                    } )
+                    .catch( function ( err ) {
+                        console.log( 'No custom routes found.' );
 
-                        route(null, $window.location.pathname);
-                        $scope.$on('$locationChangeStart', route);
-                    });
+                        route( null, $window.location.pathname );
+                        $scope.$on( '$locationChangeStart', route );
+                    } );
 
-            });
+            } );
 
 
         /**
@@ -96,14 +96,14 @@ app.component('appRouter', {
          * @arg {String} oldUrl
          * @arg {Unknown} n
          */
-        function route(ev, nextUrl, oldUrl, n) {
-            nextUrl = nextUrl.replace('://', '');
-            var path = nextUrl.split('/')
-                .slice(2);
+        function route( ev, nextUrl, oldUrl, n ) {
+            nextUrl = nextUrl.replace( '://', '' );
+            var path = nextUrl.split( '/' )
+                .slice( 2 );
 
-            if (!$scope.$root.__modules[path[0]]) {
-                console.warn('Unhandled route :', path);
-                if ($scope.$root.__module && !$scope.$root.__module.dynamic) {
+            if ( !$scope.$root.__modules[ path[ 0 ] ] ) {
+                console.warn( 'Unhandled route :', path );
+                if ( $scope.$root.__module && !$scope.$root.__module.dynamic ) {
                     $scope.$root.__module = undefined;
                 }
                 $scope.__id = undefined;
@@ -115,55 +115,55 @@ app.component('appRouter', {
                 return;
             }
 
-            $scope.$root.__module = $scope.$root.__modules[path[0]];
-            $scope.__id = path[1];
-            $scope.__view = path[2];
-            console.info('Going to', $scope.$root.__module.name, $scope.__id, $scope.__view);
+            $scope.$root.__module = $scope.$root.__modules[ path[ 0 ] ];
+            $scope.__id = path[ 1 ];
+            $scope.__view = path[ 2 ];
+            console.info( 'Going to', $scope.$root.__module.name, $scope.__id, $scope.__view );
 
-            if (!$scope.__id && !$scope.__view) {
+            if ( !$scope.__id && !$scope.__view ) {
                 $scope.item = undefined;
             }
 
             try {
-                if ($scope.$root.__module.controller && $scope.$root.__module.name != lastModule) {
+                if ( $scope.$root.__module.controller && $scope.$root.__module.name != lastModule ) {
 
-                    Object.keys($scope)
-                        .forEach(function (k) {
-                            if (k.indexOf('$') && k.indexOf('_'))
-                                delete $scope[k];
-                        });
+                    Object.keys( $scope )
+                        .forEach( function ( k ) {
+                            if ( k.indexOf( '$' ) && k.indexOf( '_' ) )
+                                delete $scope[ k ];
+                        } );
 
-                    $scope.$root.__module.ctrl = $controller($scope.$root.__module.controller, {
+                    $scope.$root.__module.ctrl = $controller( $scope.$root.__module.controller, {
                         $scope: $scope
-                    });
+                    } );
 
                     lastModule = $scope.$root.__module.name;
 
                 }
-            } catch (err) {
-                console.error(err);
+            } catch ( err ) {
+                console.error( err );
             }
 
-            $scope.openToolbox = function openToolbox(name) {
+            $scope.openToolbox = function openToolbox( name ) {
 
-                if (!$scope.$root.__module.config || !$scope.$root.__module.config.toolbox || !$scope.$root.__module.config.toolbox[name])
-                    return console.error('Toolbox', name, 'not found');
+                if ( !$scope.$root.__module.config || !$scope.$root.__module.config.toolbox || !$scope.$root.__module.config.toolbox[ name ] )
+                    return console.error( 'Toolbox', name, 'not found' );
 
-                $scope.__config = $scope.$root.__module.config.toolbox[name];
+                $scope.__config = $scope.$root.__module.config.toolbox[ name ];
                 $scope.__config.toolboxName = name;
 
-                console.info('Opening toolbox:', $scope.__config);
+                console.info( 'Opening toolbox:', $scope.__config );
 
-                if ($scope.__config.onListEdit) {
+                if ( $scope.__config.onListEdit ) {
                     $scope.__id = "new";
-                    $scope.tab('');
+                    $scope.tab( '' );
                 } else {
-                    return ngDialog.open({
+                    return ngDialog.open( {
                         template: $scope.__config.templateUrl || 'modals/toolbox.pug',
                         controller: $scope.__config.controller || 'modals/toolbox',
                         showClose: false,
                         scope: $scope
-                    });
+                    } );
                 }
 
             };
@@ -171,4 +171,4 @@ app.component('appRouter', {
         }
 
     }
-});
+} );
