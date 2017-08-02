bab5358565a6fcfa5b641b5798596644f2d7d96b
Stephane <sdemingongo@gmail.com>
2017-08-02 14:45:22 +0100

    generic/logs/history.pug

---
+++ app/generic/logs/alerts.js
@@ -0,0 +1,56 @@
+app.controller( 'generic/logs/alerts', function ( $scope, $http ) {
+
+  console.log("%cgeneric/logs/alerts","background-color: black; color: #2BFF00");
+
+    $scope.config = {};
+
+    loadConfig();
+
+    function loadConfig(){
+      if($scope.$root.__module.config
+          && $scope.$root.__module.config.tabs
+          && $scope.$root.__module.config.logs && $scope.$root.__module.config.logs.alerts){
+              $scope.options = buildConfig($scope.$root.__module.config.logs.alerts);
+          }
+          else{
+            $scope.options = buildConfig();
+          }
+          console.log("%clog/alerts loaded","background-color: black; color: #2BFF00");
+          $scope.loaded = true;
+    }
+
+
+
+    function buildConfig(options){
+      console.log("options",options);
+      options = options || {};
+
+      // display
+      $scope.config.fields = options.fields || [
+        "_id",
+        "rule",
+        "source",
+        "level",
+        "category",
+        "context",
+        "message",
+        "acknowledgedBy",
+        "acknowledgedAt",
+        "note"
+      ];
+      $scope.config.subOnly = options.subOnly || false;
+      $scope.config.arrayObject = options["[object]"] || options.arrayObject || {};
+      $scope.config.measurements = options["measurements"];
+      $scope.config.noType = true;
+
+      // params
+      $scope.config.apiPath = options.apiPath || "";
+      $scope.config.sort = options.sort || "timestamp";
+      $scope.config.limit = options.limit || 100;
+      $scope.config.order = options.order || "desc";
+      $scope.config.page = options.page || 0;
+
+      return $scope.config;
+    }
+
+} );
