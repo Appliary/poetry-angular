2ba38b02b12fc545420a0e8b33fd0fa3bcd343ce
Michaël Carrette <miccarr@me.com>
2017-09-06 09:44:09 +0200

    dbg

--- app/modals/editprofile.js
+++ app/modals/editprofile.js
@@ -11,10 +11,20 @@ app.controller( 'modals/editprofile', function ( $scope, $http, $window, $locati
     $scope.__joi = {};
     $http.put( '/__joi/api/users/me' )
         .then( function success( response ) {
-            return ( $scope.__joi = response.data.payload );
-        }, function error( err ) {} );
+            console.info( 'User edit succes', response );
+            toastr.success(
+                $filter( 'translate' )( 'The changes you made on your profile has been saved' ),
+                $filter( 'translate' )( 'Profile saved' )
+            );
+            $scope.cancel();
 
-    $rootScope.user;
+        }, function error( response ) {
+            toastr.error(
+                $filter( 'translate' )( response.data.message ),
+                $filter( 'translate' )( 'Error' )
+            );
+        } );
+
     $scope.email = "";
     $scope.password = {
         oldPassword: "",
@@ -48,7 +58,7 @@ app.controller( 'modals/editprofile', function ( $scope, $http, $window, $locati
     $scope.cancel = function cancel() {
         console.log( "Refreshing page" );
         $window.location.replace( $location.absUrl() );
-    }
+    };
 
     $scope.edit = function edit() {
         $scope.editState = {
@@ -67,57 +77,59 @@ app.controller( 'modals/editprofile', function ( $scope, $http, $window, $locati
             }, function error( response ) {
                 console.warn( 'Users edit failed', response );
                 $scope.editState.failed = true;
-            } )
+            } );
     };
 
     $scope.changePassword = function changePassword() {
 
         console.info( 'Try to change password', $scope.user, $scope.password );
 
-        if ( $scope.checkPassword( $scope.password.newPassword1 ) ) {
-            if ( $scope.password.newPassword1 == $scope.password.newPassword2 ) {
+        if ( !$scope.checkPassword( $scope.password.newPassword1 ) )
+            return toastr.error(
+                $filter( 'translate' )( "Password must have minimum 8 characters, at least 1 Uppercase Alphabet, 1 Lowercase Alphabet, 1 Number and 1 Special Character" ),
+                $filter( 'translate' )( 'Error' )
+            );
 
-                $http.post( '/api/users/changePwd', {
-                        oldPassword: $scope.password.oldPassword,
-                        newPassword: $scope.password.newPassword1
-                    } )
-                    .then( function success( response ) {
-                        console.info( 'User change pwd success', response );
-                        $scope.failedChangepwd = true;
-                        $scope.failChangepwdMsg = "Password successfully updated";
-                        $scope.cancel();
+        if ( $scope.password.newPassword1 != $scope.password.newPassword2 )
+            return toastr.error(
+                $filter( 'translate' )( 'Both password fields are not the same' ),
+                $filter( 'translate' )( 'Error' )
+            );
 
-                    }, function error( response ) {
-                        console.warn( 'Users change pwd failed', response );
-                        $scope.failedChangepwd = true;
-                        $scope.failChangepwdMsg = response.data;
-                    } )
-            } else {
-                $scope.failedChangepwd = true;
-                $scope.failChangepwdMsg = "Both new password field are not the same !";
-            }
-        } else {
-            $scope.failedChangepwd = true;
-            $scope.failChangepwdMsg = "Password must have minimum 8 characters, at least 1 Uppercase Alphabet, 1 Lowercase Alphabet, 1 Number and 1 Special Character";
-        }
+        $http.post( '/api/users/changePwd', {
+                oldPassword: $scope.password.oldPassword,
+                newPassword: $scope.password.newPassword1
+            } )
+            .then( function success( response ) {
+                toastr.success(
+                    $filter( 'translate' )( 'Your password has been changed' ),
+                    $filter( 'translate' )( 'Password changed' )
+                );
+                $scope.cancel();
 
+            }, function error( response ) {
+                toastr.success(
+                    $filter( 'translate' )( response.data.message ),
+                    $filter( 'translate' )( 'Error' )
+                );
+            } );
 
     };
 
     $scope.showEdit = function showEdit() {
         $scope.editView = true;
         $scope.pwdView = false;
-    }
+    };
 
     $scope.showChangepwd = function showChangepwd() {
         $scope.editView = false;
         $scope.pwdView = true;
-    }
+    };
 
     $scope.checkPassword = function ( password ) {
         console.log( "password to check", password );
         var re = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&#])[A-Za-z\d$@$!%*?&#]{8,}/;
         return re.test( password );
-    }
+    };
 
 } );
