1612f22a0cc701883d6474cd85adc50bd69d2cd9
Michaël Carrette <miccarr@me.com>
2017-09-11 10:44:14 +0200

    cleanup styles

--- handlers/styles.js
+++ handlers/styles.js
@@ -1,48 +1,48 @@
-const Poetry = require('poetry'),
-    sass = require('node-sass'),
-    fs = require('fs'),
-    config = require('../config'),
-    concat = require('concatenate');
+const Poetry = require( 'poetry' ),
+    sass = require( 'node-sass' ),
+    fs = require( 'fs' ),
+    config = require( '../config' ),
+    concat = require( 'concatenate' );
 
 // Get dependencies
 var dependencies = [];
-config.dependencies.forEach((dep) => {
+config.dependencies.forEach( dep => {
 
-    if (dep.indexOf('.css') != dep.length - 4) return;
+    if ( dep.indexOf( '.css' ) != dep.length - 4 ) return;
 
     try {
-        let file = require.resolve(dep);
+        let file = require.resolve( dep );
 
-        if (!file)
-            return Poetry.log.warn('Unable to solve JS depencency', dep);
+        if ( !file )
+            return Poetry.log.warn( 'Unable to solve JS depencency', dep );
 
-        dependencies.push(file);
-    } catch (err) {
-        Poetry.log.error('Unable to solve CSS depencency', dep);
+        dependencies.push( file );
+    } catch ( err ) {
+        Poetry.log.error( 'Unable to solve CSS depencency', dep );
     }
 
-});
+} );
 
 // Serve styles
-Poetry.route({
+Poetry.route( {
     method: 'GET',
     path: '/' + config.app.name + '/__metronic.css'
-}, (request, reply) => {
-    concat(dependencies, (err, res) => {
-        if (dependencies.length && err) return reply('body *{display:none} body::before{display:block;position:absolute;top:0;left:0;bottom:0;right:0;background:#A45;color:white;font-family:courrier;padding:20px;content:\'Dependency ' + err.toString() + '\';}')
-            .type('text/css');
+}, ( request, reply ) => {
+    concat( dependencies, ( err, res ) => {
+        if ( dependencies.length && err ) return reply( 'body *{display:none} body::before{display:block;position:absolute;top:0;left:0;bottom:0;right:0;background:#A45;color:white;font-family:courrier;padding:20px;content:\'Dependency ' + err.toString() + '\';}' )
+            .type( 'text/css' );
 
-        sass.render({
+        sass.render( {
             file: __dirname + '/../styles/index.scss'
-        }, (err, result) => {
+        }, ( err, result ) => {
 
-            if (err) {
-                Poetry.log.error('SASS', err);
-                return reply('body *{display:none} body::before{display:block;position:absolute;top:0;left:0;bottom:0;right:0;background:#A45;color:white;font-family:courrier;padding:20px;content:\'' + err.message + ' at ' + err.file + ':' + err.line + '\';}')
-                    .type('text/css');
+            if ( err ) {
+                Poetry.log.error( 'SASS', err );
+                return reply( 'body *{display:none} body::before{display:block;position:absolute;top:0;left:0;bottom:0;right:0;background:#A45;color:white;font-family:courrier;padding:20px;content:\'' + err.message + ' at ' + err.file + ':' + err.line + '\';}' )
+                    .type( 'text/css' );
             }
-            reply(res + result.css)
-                .type('text/css');
-        });
-    });
-});
+            reply( res + result.css )
+                .type( 'text/css' );
+        } );
+    } );
+} );
--- index.js
+++ index.js
@@ -1,115 +1,69 @@
 'use strict';
-var mime = require('mime-types');
+var mime = require( 'mime-types' );
 
-const Poetry = require('poetry'),
-    fs = require('fs'),
-    config = require('./config'),
-    angular = require('./handlers/angular');
+const Poetry = require( 'poetry' ),
+    fs = require( 'fs' ),
+    config = require( './config' ),
+    angular = require( './handlers/angular' );
 
 
 let ngcache = [];
 
-Poetry.route({
+Poetry.route( {
 
     method: 'GET',
     path: '/' + config.app.name + '/{file*}',
     config: {
         description: config.app.name,
-        tags: ['Applications (front-end)'],
+        tags: [ 'Applications (front-end)' ],
         cors: false
     }
 
-}, (request, reply) => {
+}, mainHandler );
 
-    if (~ngcache.indexOf(request.params.file))
-        return reply(angular());
+Poetry.route( {
 
-    fs.readFile('./assets/' + request.params.file, (err, file) => {
-        Poetry.log.silly(err, file);
-        if (!err)
-            return reply(file)
-                .type(mime.lookup(request.params.file));
+    method: 'GET',
+    path: '/__assets/{file*}',
+    config: {
+        description: '__Assets__',
+        tags: [ 'Applications (front-end)' ],
+        cors: false
+    }
 
-        fs.readFile('./node_modules/poetry-angular/assets/' + request.params.file, (err, file) => {
-            Poetry.log.silly(err, file);
-            if (!err)
-                return reply(file)
-                    .type(mime.lookup(request.params.file));
+}, mainHandler );
 
-            if (request.params.file && request.params.file.length > 4 &&
+function mainHandler( request, reply ) {
+
+    if ( ~ngcache.indexOf( request.params.file ) )
+        return reply( angular() );
+
+    fs.readFile( './assets/' + request.params.file, ( err, file ) => {
+        Poetry.log.silly( err, file );
+        if ( !err )
+            return reply( file )
+                .type( mime.lookup( request.params.file ) );
+
+        fs.readFile( './node_modules/poetry-angular/assets/' + request.params.file, ( err, file ) => {
+            Poetry.log.silly( err, file );
+            if ( !err )
+                return reply( file )
+                    .type( mime.lookup( request.params.file ) );
+
+            if ( request.params.file && request.params.file.length > 4 &&
                 (
-                    request.params.file.indexOf('.pug') == request.params.file.length - 4 || request.params.file.indexOf('.js') == request.params.file.length - 3
-                )) return reply()
-                .code(404);
+                    request.params.file.indexOf( '.pug' ) == request.params.file.length - 4 || request.params.file.indexOf( '.js' ) == request.params.file.length - 3
+                ) ) return reply()
+                .code( 404 );
 
-            ngcache.push(request.params.file);
-            return reply(angular());
-        });
-    });
-});
+            ngcache.push( request.params.file );
+            return reply( angular() );
+        } );
+    } );
+}
 
-Poetry.route({
-    method: 'GET',
-    path: '/assets/layouts/layout/img/{file*}',
-    config: {
-        description: config.app.name,
-        tags: ['Applications (front-end)'],
-        cors: false
-    }
-}, (request, reply) => {
-    if (~ngcache.indexOf(request.params.file))
-        return reply(angular());
-
-    fs.readFile('./assets/img/' + request.params.file, (err, file) => {
-        Poetry.log.silly(err, file);
-        if (!err)
-            return reply(file)
-                .type(mime.lookup(request.params.file));
-
-        fs.readFile('./node_modules/poetry-angular/assets/img/' + request.params.file, (err, file) => {
-            Poetry.log.silly(err, file);
-            if (!err)
-                return reply(file)
-                    .type(mime.lookup(request.params.file));
-        });
-    });
-});
-
-Poetry.route({
-    method: 'GET',
-    path: '/assets/layouts/layout/css/themes/{file*}',
-    config: {
-        description: config.app.name,
-        tags: ['Applications (front-end)'],
-        cors: false
-    }
-}, (request, reply) => {
-    if (~ngcache.indexOf(request.params.file))
-        return reply(angular());
-
-      //- theme files
-      fs.readFile('./styles/metronic/layouts/layout/themes/' + request.params.file, (err, file) => {
-            Poetry.log.silly(err, file);
-            if (!err)
-                return reply(file)
-                    .type(mime.lookup(request.params.file));
-        // for img files
-        fs.readFile('./node_modules/poetry-angular/assets/img/' + request.params.file, (err, file) => {
-            Poetry.log.silly(err, file);
-            if (!err)
-                return reply(file)
-                    .type(mime.lookup(request.params.file));
-        });
-      });
-});
-
-require('./handlers/javascripts');
-require('./handlers/templates');
-require('./handlers/styles');
-require('./handlers/devProxy');
-require('./register');
-
-//global.jQuery = global.$ = require('jquery');
-require('jquery');
-//require('bootstrap');
-require('js-cookie');
+require( './handlers/javascripts' );
+require( './handlers/templates' );
+require( './handlers/styles' );
+require( './handlers/devProxy' );
+require( './register' );
